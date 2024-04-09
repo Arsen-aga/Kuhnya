b38a0ca6f748a624adbd5515883eeec953f83310
Arsen Asanov <tatarin08082002@gmail.com>
2024-04-09 18:47:31 +0300

    add front-page

--- app/js/accomodate/main.js
+++ app/js/accomodate/main.js
@@ -1,86 +1,292 @@
-//= ../libs/slick.min.js
-//= ../libs/fancybox.umd.js
-
-$(".services__slider").slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 2,
-      },
-    },
-    {
-      breakpoint: 850,
-      settings: {
-        slidesToShow: 2,
-        arrows: false,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 568,
-      settings: {
-        arrows: false,
-        dots: true,
-        slidesToShow: 1,
-      },
-    },
-  ],
+const swiper = new Swiper(".slider", {
+  loop: true,
+  slidesPerView: 4,
+  spaceBetween: 12,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2000,
+  },
 });
 
-if($(".reviews__slider")){
-  $(".reviews__slider").slick({
-  infinite: false,
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  responsive: [
-    {
-      breakpoint: 992,
-      settings: {
-        slidesToShow: 3,
-      },
-    },
-    {
-      breakpoint: 850,
-      settings: {
-        slidesToShow: 3,
-        arrows: false,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 568,
-      settings: {
-        slidesToShow: 2,
-        arrows: false,
-        dots: true,
-      },
-    },
-    {
-      breakpoint: 421,
-      settings: {
-        slidesToShow: 1,
-        arrows: false,
-        dots: true,
-      },
-    },
-  ],
-});
-}
+Fancybox.bind("[data-fancybox]", {});
 
-
-if ($(".questions__item")) {
-  $(".questions__item").on("click", function () {
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
-    } else {
-      $(".questions__item").removeClass("active");
-      $(this).addClass("active");
-    }
+function getSliderItem(swiperTop, thumbSwiper) {
+  if (!(swiperTop && thumbSwiper)) return;
+  const swiperThumb = new Swiper(thumbSwiper, {
+    spaceBetween: 8,
+    slidesPerView: 4,
+    freeMode: true,
+    watchSlidesProgress: true,
+    initialSlide: 5,
+  });
+  const swiper = new Swiper(swiperTop, {
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiperThumb,
+    },
   });
 }
 
-Fancybox.bind("[data-fancybox]", {});
+if (document.querySelectorAll(".swiper-top")) {
+  const swiperTop = document.querySelectorAll(".swiper-top");
+  const thumbSwiper = document.querySelectorAll(".thumb-swiper");
+
+  if (swiperTop.length === 1) {
+    getSliderItem(swiperTop, thumbSwiper);
+  }
+  for (let i = 0; i < swiperTop.length; i++) {
+    getSliderItem(swiperTop[i], thumbSwiper[i]);
+  }
+}
+
+// document.addEventListener("DOMContentLoaded", () => {
+  const wrapper = document.querySelectorAll(".slider-items__item-slider");
+  wrapper.forEach((w) => {
+    const elems = w.querySelectorAll(".slider-items__thumb");
+    const prev = w.querySelector(".swiper-button-prev");
+    const next = w.querySelector(".swiper-button-next");
+    prev.addEventListener("click", checkCoordSlide(elems, 3, "prev"));
+    next.addEventListener("click", ()=>{
+      let count = 0;
+      checkCoordSlide(elems, 3, "next", count);
+    });
+    checkCoordSlide(elems, 3);
+  });
+// });
+
+function checkCoordSlide(elems, index, value = "next") {
+  let rest = elems.length - 4;
+
+
+  
+
+  elems.forEach((elem) => {
+    console.log(elem.offsetLeft);
+    console.log(+elem.style.width.replace("px", "") + 8 + elem.offsetLeft);
+
+
+    if (elem.querySelector(".num-photo"))
+      elem.querySelector(".num-photo").remove();
+  });
+  elems[index].insertAdjacentHTML("beforeend", getRestThePicture(rest));
+}
+
+function getRestThePicture(num) {
+  return `
+    <div class="num-photo">
+      <p class="num-photo__text">+ <span class="num-photo__num">${num}</span> фото</p>
+    </div>
+  `;
+}
+
+if (document.querySelector(".filters-select__select")) {
+  const selectContainers = document.querySelectorAll(".filters-select__select");
+
+  selectContainers.forEach((container) => {
+    const header = container.querySelector(".filters-select__select-header");
+    const list = container.querySelector(".filters-select__select-list");
+    const inputs = list.querySelectorAll("input");
+
+    let initialHeaderText = header.textContent.trim(); // Сохраняем изначальное значение заголовка
+
+    header.addEventListener("click", function () {
+      if (list.style.display === "none" || list.style.display === "") {
+        closeOtherSelects();
+        list.style.display = "flex";
+        header.classList.add("_active");
+      } else {
+        list.style.display = "none";
+        header.classList.remove("_active");
+      }
+    });
+
+    inputs.forEach((input) => {
+      input.addEventListener("change", function () {
+        let selectedTexts = Array.from(inputs)
+          .filter((input) => input.checked)
+          .map((input) => {
+            const label = input.nextElementSibling; // Находим следующий элемент - метку (label)
+            return label.textContent.trim(); // Возвращаем текст метки
+          })
+          .join(", ");
+
+        header.textContent = selectedTexts || initialHeaderText; // Устанавливаем текст заголовка
+      });
+    });
+  });
+
+  function closeOtherSelects() {
+    selectContainers.forEach((container) => {
+      const list = container.querySelector(".filters-select__select-list");
+      const header = container.querySelector(".filters-select__select-header");
+      if (list.style.display === "flex") {
+        list.style.display = "none";
+        header.classList.remove("_active");
+      }
+    });
+  }
+  document.addEventListener("click", function (event) {
+    const isClickInsideFilter = event.target.closest(".filters-select");
+    if (!isClickInsideFilter) {
+      // Клик был сделан вне .filters
+      closeAllSelects();
+    }
+  });
+
+  function closeAllSelects() {
+    selectContainers.forEach((container) => {
+      const list = container.querySelector(".filters-select__select-list");
+      const header = container.querySelector(".filters-select__select-header");
+      if (list.style.display === "flex") {
+        list.style.display = "none";
+        header.classList.remove("_active");
+      }
+    });
+  }
+}
+
+if (document.querySelector(".slider-items__item-accordion")) {
+  const accordWrappers = document.querySelectorAll(
+    ".slider-items__item-accordion"
+  );
+
+  accordWrappers.forEach((accordWrapper) => {
+    const text = accordWrapper.querySelector(".slider-items__item-style");
+    const btn = accordWrapper.querySelector(".slider-items__item-more");
+    const btnText = btn.querySelector("span");
+
+    btn.addEventListener("click", () => {
+      text.classList.toggle("active");
+      btnText.textContent =
+        btnText.textContent === "Открыть" ? "Закрыть" : "Открыть";
+    });
+  });
+}
+
+// new mask
+const phoneInputs = document.querySelectorAll('input[type="tel"]');
+if (phoneInputs && phoneInputs.length) {
+  const getInputsNumbersValue = function (input) {
+    return input.value.replace(/\D/g, "");
+  };
+
+  const onPhoneInput = function (e) {
+    let input = e.target,
+      inputNumbersValue = getInputsNumbersValue(input),
+      formattedInputValue = "",
+      selectionStart = input.selectionStart;
+
+    if (!inputNumbersValue) {
+      return (input.value = "");
+    }
+
+    if (input.value.length != selectionStart) {
+      if (e.data && /\D/g.test(e.data)) {
+        inputNumbersValue[1] =
+          inputNumbersValue[1] == "8"
+            ? (inputNumbersValue[1] = "")
+            : inputNumbersValue[1];
+        input.value = inputNumbersValue;
+      }
+      return;
+    }
+
+    if (["7", "8", "9"].indexOf(inputNumbersValue[0]) > -1) {
+      // Russian phone number
+      if (inputNumbersValue[0] == "9")
+        inputNumbersValue = "7" + inputNumbersValue;
+      const firstSymbols = inputNumbersValue[0] == "8" ? "8" : "+7";
+      formattedInputValue = firstSymbols + " ";
+      if (inputNumbersValue.length > 1) {
+        formattedInputValue +=
+          "(" +
+          (inputNumbersValue[1] == "8"
+            ? (inputNumbersValue[1] = "")
+            : inputNumbersValue[1]) +
+          inputNumbersValue.substring(2, 4);
+      }
+      if (inputNumbersValue.length >= 5) {
+        formattedInputValue += ") " + inputNumbersValue.substring(4, 7);
+      }
+      if (inputNumbersValue.length >= 8) {
+        formattedInputValue += "-" + inputNumbersValue.substring(7, 9);
+      }
+      if (inputNumbersValue.length >= 10) {
+        formattedInputValue += "-" + inputNumbersValue.substring(9, 11);
+      }
+    } else {
+      // Not Russian phone number
+      formattedInputValue = "+" + inputNumbersValue.substring(0, 16);
+    }
+    input.value = formattedInputValue;
+  };
+
+  const onPhoneKeyDown = function (e) {
+    const input = e.target;
+    if (e.keyCode == 8 && getInputsNumbersValue(input).length === 1) {
+      input.value = "";
+    }
+  };
+
+  const onPhonePaste = function (e) {
+    const input = e.target,
+      pasted = e.clipboardData || window.clipboardData,
+      inputNumbersValue = getInputsNumbersValue(input);
+
+    if (pasted) {
+      const pastedText = pasted.getData("Text");
+      if (!/\D/g.test(pastedText)) {
+        input.value = inputNumbersValue;
+      }
+    }
+  };
+
+  const onPhoneClick = function (e) {
+    const input = e.target;
+    input.setSelectionRange(4, 4);
+  };
+
+  phoneInputs.forEach((input) => {
+    input.setAttribute("maxlength", "18");
+    input.addEventListener("input", onPhoneInput);
+    input.addEventListener("keydown", onPhoneKeyDown);
+    input.addEventListener("paste", onPhonePaste);
+    input.addEventListener("click", onPhoneClick);
+  });
+}
+// new mask
+
+if(document.querySelectorAll(".filters")){
+  const filters = document.querySelectorAll(".filters");
+  
+  filters.forEach((filter) => {
+    const filterBtns = filter.querySelectorAll(".filters__btn");
+    const filterItems = filter.querySelectorAll(".filters__item");
+  
+    filterBtns.forEach(function (btn) {
+      btn.addEventListener("click", function (e) {
+        filterBtns.forEach((btn) => btn.classList.remove("active"));
+        btn.classList.add("active");
+  
+        const dataFilter = e.currentTarget.dataset.filter;
+  
+        filterItems.forEach(function (item) {
+          if (dataFilter === "all") {
+            item.classList.remove('hidden');
+            return;
+          }
+  
+          if (item.classList.contains(dataFilter)) {
+            item.classList.remove("hidden");
+          } else {
+            item.classList.add("hidden");
+          }
+        });
+      });
+    });
+  });
+}
